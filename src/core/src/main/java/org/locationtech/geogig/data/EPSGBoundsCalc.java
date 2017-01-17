c175664ecd518bb495ef3607f8b02c8a0014369c
goudine <agoudine@boundlessgeo.com>
2017-01-17 11:27:17 -0800

    Fix returned bounds
    Get correct bounds using GeneralEnvelope, but returning Envelope
    
    Signed-off-by: goudine <agoudine@boundlessgeo.com>

--- src/core/src/main/java/org/locationtech/geogig/data/EPSGBoundsCalc.java
+++ src/core/src/main/java/org/locationtech/geogig/data/EPSGBoundsCalc.java
@@ -3,13 +3,9 @@ package org.locationtech.geogig.data;
 /**
  * Created by mthompson on 2017-01-11.
  */
-import static com.google.common.base.Optional.absent;
 
-import java.util.Collection;
-import java.util.Set;
-
+import com.vividsolutions.jts.geom.Envelope;
 import org.geotools.geometry.GeneralEnvelope;
-import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
 import org.opengis.metadata.extent.Extent;
@@ -19,40 +15,35 @@ import org.opengis.referencing.crs.CRSAuthorityFactory;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.CoordinateOperation;
 import org.opengis.referencing.operation.CoordinateOperationFactory;
-import org.opengis.referencing.operation.MathTransform;
 
-import com.google.common.base.Optional;
-import com.vividsolutions.jts.geom.Envelope;
+import java.util.Collection;
+import java.util.Set;
 
 public class EPSGBoundsCalc {
 
 
-    public Optional<Envelope> getExtents(CoordinateReferenceSystem crs, StringBuilder outErr) {
+    public Envelope getExtents(CoordinateReferenceSystem crs) throws Exception {
 
         final Extent domainOfValidity = crs.getDomainOfValidity();
 
         if (null == domainOfValidity) {
-            outErr.append("No domain of validity provided by CRS definition");
-            return absent();
+            throw new Exception("No domain of validity provided by CRS definition");
         }
 
         Collection<? extends GeographicExtent> geographicElements;
         geographicElements = domainOfValidity.getGeographicElements();
 
         if (null == geographicElements || geographicElements.size() != 1) {
-            outErr.append("Number of geographic elements != 1");
-            return absent();
+            throw new Exception("Number of geographic elements != 1");
         }
 
         GeographicExtent geographicExtent = geographicElements.iterator().next();
         if (!(geographicExtent instanceof GeographicBoundingBox)) {
-            outErr.append("geographic extent is not a geographic bounding box");
-            return absent();
+            throw new Exception("geographic extent is not a geographic bounding box");
         }
 
         if (!geographicExtent.getInclusion()) {
-            outErr.append("geographic extent is exclusive, can only deal with inclusive ones");
-            return absent();
+            throw new Exception("geographic extent is exclusive, can only deal with inclusive ones");
         }
 
         GeographicBoundingBox geographicBoundingBox = (GeographicBoundingBox) geographicExtent;
@@ -63,42 +54,35 @@ public class EPSGBoundsCalc {
         double maxy = geographicBoundingBox.getNorthBoundLatitude();
 
         CoordinateReferenceSystem wgs84LongFirst;
-        CoordinateReferenceSystem targetCRS;
-
         try {
 
             wgs84LongFirst = CRS.decode("EPSG:4326", true);
-            MathTransform mathTransform = CRS.findMathTransform(wgs84LongFirst, crs, true);
-
-            Envelope wgs84Envelope = new Envelope(minx, maxx, miny, maxy);
-
-
             CoordinateOperationFactory coordOpFactory = CRS.getCoordinateOperationFactory(true);
             CoordinateOperation op = coordOpFactory.createOperation(wgs84LongFirst,crs);
 
-            ReferencedEnvelope envelope = new ReferencedEnvelope(minx, maxx, miny, maxy, wgs84LongFirst);
-            GeneralEnvelope g = CRS.transform(op, envelope);
+            ReferencedEnvelope refEnvelope = new ReferencedEnvelope(minx, maxx, miny, maxy, wgs84LongFirst);
+            GeneralEnvelope genEnvelope = CRS.transform(op, refEnvelope);
 
-            Envelope crsBounds = JTS.transform(wgs84Envelope, mathTransform);
+            double xmax = genEnvelope.getMaximum(0);
+            double ymax = genEnvelope.getMaximum(1);
+            double xmin = genEnvelope.getMinimum(0);
+            double ymin = genEnvelope.getMinimum(1);
 
-            return Optional.of(crsBounds);
-
+            Envelope envelope = new Envelope(xmin, xmax, ymin, ymax);
+            return envelope;
         } catch (Exception e) {
-            outErr.append("ERROR: " + e.getMessage());
-            return absent();
+            throw new Exception("ERROR: " + e.getMessage());
         }
     }
 
-    public Optional<Envelope> findCode(String refId) throws Exception {
-        Optional<Envelope> projectionBounds = Optional.absent();
+    public Envelope findCode(String refId) throws Exception {
+        Envelope projectionBounds = null;
         CoordinateReferenceSystem crs;
 
         CRSAuthorityFactory authorityFactory = CRS.getAuthorityFactory(true);
-        Set<String> authorityCodes = authorityFactory
-            .getAuthorityCodes(CoordinateReferenceSystem.class);
+        Set<String> authorityCodes = authorityFactory.getAuthorityCodes(CoordinateReferenceSystem.class);
 
         for (String code : authorityCodes) {
-            //only checks the EPSG codes
             if (code.startsWith("EPSG:") && code.contains(refId)) {
                 try {
                     crs = authorityFactory.createCoordinateReferenceSystem(code);
@@ -106,9 +90,8 @@ public class EPSGBoundsCalc {
                     System.err.printf("%s: Unable to create CRS: %s\n", code, e.getMessage());
                     continue;
                 }
-                StringBuilder err = new StringBuilder();
-                projectionBounds = getExtents(crs, err);
-                System.err.printf("%s: , %s , %s\n", code, projectionBounds.orNull(), err);
+                projectionBounds = getExtents(crs);
+                System.err.printf("%s: , %s \n", code, projectionBounds);
             }
         }
         return projectionBounds;
--- src/core/src/test/java/org/locationtech/geogig/data/EPSGBoundsCalcTest.java
+++ src/core/src/test/java/org/locationtech/geogig/data/EPSGBoundsCalcTest.java
@@ -50,8 +50,6 @@ public class EPSGBoundsCalcTest extends RepositoryTestCase {
                 code = ((GeometryDescriptor) desc).getCoordinateReferenceSystem().getName();
             }
         }
-        System.out.println(code);
-        System.out.println("  ->  This is the code variable\n");
 
         String[] testArray = {"EPSG:4326","EPSG:26910","EPSG:3857","EPSG:3412","EPSG:3411"};
 
